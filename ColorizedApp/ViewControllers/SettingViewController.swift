9a026ae873192f630aeec156470f13630eeb737f
Isa199510 <isa.arbuhov@mail.ru>
2022-11-10 09:05:37 +0600

    setup

---
+++ ColorizedApp/ViewControllers/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  ColorizedApp
+//
+//  Created by Иса on 10.11.2022.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
